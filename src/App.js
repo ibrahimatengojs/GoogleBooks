18bb9110d09380312b615e2d4b5663e46091f984
Meghna <meggie@Rajs-MacBook-Pro.local>
2018-01-12 16:43:25 +0100

    used react-bootstrap ui to create header and books component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
-import Header from './Components/Header';
+import { Grid, Col, Row } from 'react-bootstrap';
 import './App.css';
+import Header from './Components/Header';
+import Books from './Components/Books';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Grid>
+          <Row>
+            <Col xs={12} md={8} lg={8}>
+              <Books />
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
---
+++ src/Components/Books.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {Navbar} from 'react-bootstrap';
+
+class Books extends Component {
+  render() {
+    return (
+        <div>Books</div>
+    );
+  }
+}
+
+export default Books;
